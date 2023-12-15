0d895ff5ce10275326276248fb237c3575b47e18
AinazikB <145146792+AinazikB@users.noreply.github.com>
2023-12-15 23:31:00 +0600

    Update main.js

--- main.js
+++ main.js
@@ -24,3 +24,31 @@ const inputBox = document.getElementById("username");
         e.target.parentElement.remove()
     };
  }, false);
+function displayTasks() {
+        fetch(apiToPost)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Network response was not ok: ${response.statusText}`);
+                }
+                return response.json();
+            })
+            .then(tasks => {
+                tasks.forEach(task => {
+                    let li = document.createElement("li");
+                    li.innerHTML = task.title;
+                    li.id = task.id;
+                    if (task.completed) {
+                        li.classList.add("checked");
+                    }
+    
+                    let span = document.createElement("span");
+                    span.innerHTML = "\u00d7";
+                    li.appendChild(span);
+                    listContainer.appendChild(li);
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching tasks:', error);
+            });
+    }
+    window.onload = displayTasks;
